b7fd359c6b51223690580a4d01a8da2d7d83a6a4
YihengYao <fn1117@qq.com>
2017-10-20 23:46:37 +1300

    17_10_20_移动飞机
    使用Mathf.Clamp限制刚体的值在给定的范围之间
    限制value的值在min和max之间， 如果value小于min，返回min。 如果value大于max，返回max，否则返回value.
    使用Quaternion.Euler返回一个rotation, 绕指定的轴旋转

---
+++ Space Shooter/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//通过添加该语句使该类能在外界访问
+//可序列化.
+[System.Serializable]
+public class Boundary{
+    public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody rigidBody;
+    public float speed;
+    public Boundary boundary;
+
+    //斜度
+    public float tilt;
+
+	// Use this for initialization
+	void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+	}
+
+    private void FixedUpdate()
+    {
+        //从键盘获取输入
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+
+		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        //设置刚体的移动速率
+        rigidBody.velocity = movement * speed;
+
+        //设置刚体的位置, 不能使其跑出屏幕外
+        //使用Mathf.Clamp限制刚体的值在给定的范围之间
+        rigidBody.position = new Vector3(Mathf.Clamp(rigidBody.position.x, boundary.xMin, boundary.xMax),
+                                         0.0f,
+                                         Mathf.Clamp(rigidBody.position.z, boundary.zMin, boundary.zMax)
+                                        );
+        //设置飞机移动时, 产生的倾斜
+        rigidBody.rotation = Quaternion.Euler(0.0f, 0.0f, rigidBody.velocity.x * -tilt);
+
+	}
+}
