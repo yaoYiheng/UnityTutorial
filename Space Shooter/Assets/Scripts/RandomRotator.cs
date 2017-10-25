e7498db90342f4908ffcf59bf2ee9619017fbdd4
YihengYao <fn1117@qq.com>
2017-10-25 21:44:24 +1300

    17_10_25_子弹与物体的碰撞检测
    注意点: 因为物体与边界也会调用OnTriggerEnter方法, 所以需要通过给边界绑定标签的方法, 来避免一开始就使物体消失的bug.
    
    添加一个GameObject, 实现游戏逻辑, 将artwork添加到该GameObjcect上作为其child.

---
+++ Space Shooter/Assets/Scripts/RandomRotator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotator : MonoBehaviour {
+
+    public float tumble;
+
+    private Rigidbody rigidBody;
+    // Use this for initialization
+    private void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+
+        rigidBody.angularVelocity = Random.insideUnitSphere * tumble;
+    }
+
+    //检测到碰撞时, 销毁物体, 以及子弹.
+    private void OnTriggerEnter(Collider other)
+    {
+        //游戏一开始时, Boundary与物体就会有接触, 便会调用该方法, 绑定一个tag避免
+        if (other.tag == "Boundary") return;
+        //检查到碰撞后, 销毁相关碰撞物.
+        Destroy(gameObject);
+        Destroy(other.gameObject);
+    }
+}
