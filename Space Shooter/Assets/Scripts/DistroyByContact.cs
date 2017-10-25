913b5eb2b3b5806abf94426ce0dcf41b00682014
YihengYao <fn1117@qq.com>
2017-10-25 21:49:28 +1300

    17_10_25_回顾教程, 发现把碰撞检测写到了另一个脚本文件中

---
+++ Space Shooter/Assets/Scripts/DistroyByContact.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DistroyByContact : MonoBehaviour {
+
+	//检测到碰撞时, 销毁物体, 以及子弹.
+	private void OnTriggerEnter(Collider other)
+	{
+		//游戏一开始时, Boundary与物体就会有接触, 便会调用该方法, 绑定一个tag避免
+		if (other.tag == "Boundary") return;
+		//检查到碰撞后, 销毁相关碰撞物.
+		Destroy(gameObject);
+		Destroy(other.gameObject);
+	}
+}
