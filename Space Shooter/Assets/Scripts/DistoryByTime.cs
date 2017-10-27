c6cf6dbdf5947ce5e33d4c7609c4cfe7411e362b
YihengYao <fn1117@qq.com>
2017-10-28 09:17:42 +1300

    17_10_28_设置陨石间隔
    通过使用WaitForSeconds()来实现添加游戏间的间隔作用, 该方法所在的函数的返回值必须得是IEnumerator, 并且需要与yield return new WaitForSeconds()连用, 并且所有IEnumerator类型函数必须使用”StartCoroutine”这个函数触发，不能单独使用.
    
    在通过while与for循环实现每个陨石间的间隔与没波陨石的间隔.
    
    爆炸后的对象依然存在, 新添加一个DestoryByTime的脚本, 并将其添加到爆炸效果的Prefab里.

---
+++ Space Shooter/Assets/Scripts/DistoryByTime.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DistoryByTime : MonoBehaviour {
+
+    public float lifeTime;
+
+    private void Start()
+    {
+        Destroy(gameObject, lifeTime);
+    }
+}
--- Space Shooter/Assets/Scripts/GameController.cs
+++ Space Shooter/Assets/Scripts/GameController.cs
@@ -7,10 +7,21 @@ public class GameController : MonoBehaviour
     public GameObject spawn;
     public Vector3 spawnValue;
 
+    //每一波出多少个
+    public float spawnCount;
+    //每个陨石间隔多久
+    public float spawnWait;
+
+    //留有一定的时间可供玩家就绪
+    public float startWait;
+
+    //每一波之间的间隔
+    public float spawnWaves;
     // Use this for initialization
     void Start()
     {
-        spawnWave();
+        //所有IEnumerator类型函数必须使用”StartCoroutine”这个函数触发，不能单独使用
+        StartCoroutine(spawnWave());
     }
 
     // Update is called once per frame
@@ -19,10 +30,26 @@ public class GameController : MonoBehaviour
 
     }
 
-    void spawnWave(){
+	//所有使用yield的函数必须将返回值类型设置为IEnumerator类型
+	IEnumerator spawnWave(){
 
-        Vector3 spawnPosition = new Vector3(Random.Range(-spawnValue.x, spawnValue.x), spawnValue.y, spawnValue.z);
-        Quaternion rotation = Quaternion.identity;
-        Instantiate(spawn, spawnPosition, rotation);
+        //开始游戏时, 用于等待玩家就绪
+        yield return new WaitForSeconds(startWait);
+
+        while(true){
+            
+			for (int i = 0; i < spawnCount; i++){
+				Vector3 spawnPosition = new Vector3(Random.Range(-spawnValue.x, spawnValue.x), spawnValue.y, spawnValue.z);
+				Quaternion rotation = Quaternion.identity;//no rotation
+				Instantiate(spawn, spawnPosition, rotation);
+				
+				yield return new WaitForSeconds(spawnWait);
+				
+			}
+
+            //每一波陨石间的间隔.
+            yield return new WaitForSeconds(spawnWaves);
+        }
+
     }
 }
