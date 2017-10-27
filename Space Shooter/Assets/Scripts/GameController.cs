1e1e453234b8ec780678a0614e320f693975abd1
YihengYao <fn1117@qq.com>
2017-10-27 23:03:07 +1300

    17_10_27_添加游戏控制器
    添加一个空的GameObject用来作为游戏控制器, 掌管相关逻辑的实现, 比如控制陨石的坠落等等.
    
    在Hierarchy中添加一个新的Gameobject, 添加脚本, 编写一个用来生成陨石的方法spawnWave(), 并在Start()方法中调用该方法.
    需要在spawnWave()中进行陨石的初始化, 使用Instantiate()方法, 该方法的三个参数:
     第一个参数, 待实例化的对象, 用一个公共的变量接收来自外界传入的对象. 第二个参数为实例化对象的位置, 这里希望它随机出现在的位置, 所以通过使用Random.Range()方法在给定范围内返回一个随机值, 作为实例化对象的x值.
    第三个参数Quaternion()使用Quaternion.identity

---
+++ Space Shooter/Assets/Scripts/GameController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject spawn;
+    public Vector3 spawnValue;
+
+    // Use this for initialization
+    void Start()
+    {
+        spawnWave();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void spawnWave(){
+
+        Vector3 spawnPosition = new Vector3(Random.Range(-spawnValue.x, spawnValue.x), spawnValue.y, spawnValue.z);
+        Quaternion rotation = Quaternion.identity;
+        Instantiate(spawn, spawnPosition, rotation);
+    }
+}
