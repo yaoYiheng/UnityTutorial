763de491761c84d4ae6c75529f8633f1dde6dd75
YihengYao <fn1117@qq.com>
2017-11-03 00:15:45 +1300

    17_11_03_实现背景的无缝连接循环
            float newPostion = Mathf.Repeat(Time.time * scrollSpeed, tiltSizeZ);
            transform.position = startPostion + Vector3.forward * newPostion;

---
+++ Space Shooter/Assets/Scripts/BGScroller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGScroller : MonoBehaviour {
+
+    public float scrollSpeed;
+    public float tiltSizeZ;
+
+    private Vector3 startPostion;
+	
+    // Use this for initialization
+	void Start () {
+        startPostion = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float newPostion = Mathf.Repeat(Time.time * scrollSpeed, tiltSizeZ);
+        transform.position = startPostion + Vector3.forward * newPostion;
+	}
+}
