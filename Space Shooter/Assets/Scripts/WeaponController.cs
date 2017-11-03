0a01062241e3c29b051810e5bffa0696c389efbb
YihengYao <fn1117@qq.com>
2017-11-03 23:00:27 +1300

    17_11_03_添加enemy飞船

---
+++ Space Shooter/Assets/Scripts/WeaponController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponController : MonoBehaviour {
+
+    public GameObject shot;
+    private AudioSource audioSource;
+
+    public Transform shotSpawn;
+	// Use this for initialization
+	void Start () {
+        audioSource = GetComponent<AudioSource>();
+
+	}
+
+    void Fire() {
+        Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+        audioSource.Play();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
