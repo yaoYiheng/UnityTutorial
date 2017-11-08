99943492ee81e3194715c2297af1169b3abdf59d
YihengYao <fn1117@qq.com>
2017-11-08 23:56:55 +1300

    17_11_08_完善敌机移动脚本
    关于如何在多次重复执行计算刚体角速度的那段代码, 还需要搞清楚StartCoroutine, 以及该方法调用的方法的执行顺序.
    
    感觉: 就是Hierarchy中完成对游戏对象的设置后, 将其拖拽到Project视图中作为Prefab文件, 以供后续使用,  不知道这样理解是否正确: 从Project视图中拖拽相关素材到Hierarchy视图中, 完成刚体 碰撞器, 脚本控制相关行为等相关属性的设置后将其拖拽到Project视图后移除, (将该Prefab作为GameController障碍物的一员, 拖拽到数组中)

--- Space Shooter/Assets/Scripts/EvasiveManenver.cs
+++ Space Shooter/Assets/Scripts/EvasiveManenver.cs
@@ -5,18 +5,26 @@ using UnityEngine;
 public class EvasiveManenver : MonoBehaviour {
 
     //该变量用来使enemy并不在一开始时就移动
+	public float dodge;//help us pick targetManeuver
+	public float smoothing;
+	public float tilt;
+
     public Vector2 startWait;
-    public float dodge;//help us pick targetManeuver
 
     public Vector2 maneuverTime;
     public Vector2 maneuverWait;
 
+    public Boundary boundary;
 
     private float targetManeuver;
+    private Rigidbody rb;
+    private float currentSpeed;
 
 	void Start () {
         //执行想要重复的方法
         StartCoroutine(Evade());
+        rb = GetComponent<Rigidbody>();
+        currentSpeed = rb.velocity.z;
 	}
 	
     //需要重复执行躲避的方法
@@ -27,15 +35,29 @@ public class EvasiveManenver : MonoBehaviour {
         while(true){
 			// Mathf.Sign()判断输入的正负，如果是正则返回0或1如果是负则返回-1 
 			targetManeuver = Random.Range(1, dodge) * -Mathf.Sign(transform.position.x);
-            yield return new WaitForSeconds(maneuverTime.x, maneuverTime.y);
+            yield return new WaitForSeconds(Random.Range(maneuverTime.x, maneuverTime.y));
             targetManeuver = 0;
-            yield return new WaitForSeconds();
+            yield return new WaitForSeconds(Random.Range(maneuverWait.x, maneuverWait.y));
 
         }
     }
 
 
-	void Update () {
-        float newManenver = Mathf.MoveTowards();
+	void FixedUpdate () {
+        //Change the position
+        float newManenver = Mathf.MoveTowards(rb.velocity.x, targetManeuver, Time.deltaTime * smoothing);
+
+        //更新刚体的角速度
+        rb.velocity = new Vector3(newManenver, 0.0f, currentSpeed);
+
+        //限定刚体的移动范围, 通过使用Mathf.Clamp.
+        rb.position = new Vector3(
+            Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax),
+            0.0f,
+            Mathf.Clamp(rb.position.z, boundary.zMin, boundary.zMax)
+
+        );
+        //根据在x方向的角速度产生倾斜.
+        rb.rotation = Quaternion.Euler(0.0f, 0.0f, rb.velocity.x * -tilt);
 	}
 }
