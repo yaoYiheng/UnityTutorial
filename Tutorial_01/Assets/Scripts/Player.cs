2a96e6b931e9e3d45527bccf9429aafb118b3a93
YihengYao <fn1117@qq.com>
2017-11-09 23:51:46 +1300

    17_11_09_点击鼠标左键触发跳跃
    (Input.GetMouseButtonDown(0))//点击鼠标左键触发
    
    修改刚体的velocity
    rigidBody.velocity = Vector3.up * jumpSpeed;

---
+++ Tutorial_01/Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float jumpSpeed;
+
+    private Rigidbody rigidBody;
+	// Use this for initialization
+	void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))//点击鼠标左键触发
+            rigidBody.velocity = Vector3.up * jumpSpeed;
+	}
+}
